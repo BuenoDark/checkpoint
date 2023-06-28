ab5a9591051e83ab7ece0a3ddf447a20a1e3384b
Wesley Bueno <81834286+BuenoDark@users.noreply.github.com>
2023-06-28 20:43:56 -0300

    Update botao-favorito.componente.tsx

--- rickandmorty/src/componentes/botoes/botao-favorito.componente.tsx
+++ rickandmorty/src/componentes/botoes/botao-favorito.componente.tsx
@@ -2,14 +2,7 @@
 import { AiOutlineStar, AiFillStar } from "react-icons/ai"
 import './favorito.css'
 import { BotoesProps } from "./botoes.types";
-/**
- * Botão que indica se um elemento é favorito ou não, e dá a possibilidade de marcá-lo/desmarcá-lo
- *
- * Terá que tipar as propriedades se utilizar este componente
- *
- *
- * @returns Elemento JSX
- */
+
 const BotaoFavorito = (props: BotoesProps) => {
   const { isFavorito, onClick } = props
   const src = isFavorito ? <AiFillStar /> : <AiOutlineStar />;
